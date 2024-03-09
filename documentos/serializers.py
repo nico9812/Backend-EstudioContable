b8bc705555a3825a0b4b064545bb2bd632d76667
Barco Exequiel <exebarco280320@gmail.com>
2024-03-09 00:44:08 -0300

    fe333

--- documentos/serializers.py
+++ documentos/serializers.py
@@ -2,12 +2,18 @@ from rest_framework import serializers
 from .models import DocumentoPDF
 from .models import Categoria
 
+
 class CategoriaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Categoria
         fields = ('id', 'nombre')
 
+
 class DocumentoPDFSerializer(serializers.ModelSerializer):
+    categoria = serializers.CharField(
+        source='categoria.nombre', read_only=True)
+
     class Meta:
         model = DocumentoPDF
-        fields = '__all__'
+        fields = ['id', 'nombre', 'archivo',
+                  'propietario', 'categoria', 'fecha_creacion']
