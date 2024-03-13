f9049c2afe22e538091af06bd1cd3fdcd2c0ddfc
nico9812 <nicocalderon9818@gmail.com>
2024-03-13 02:57:01 -0300

    [add] anio y mes filtrado

--- vencimientos/views.py
+++ vencimientos/views.py
@@ -17,19 +17,37 @@ class VencimientosUsuarioView(APIView):
     authentication_classes = [ExpiringTokenAuthentication, ]
     permission_classes = [permissions.IsAuthenticated, ]
 
-    def get(self, request, usuario_id):
+    def get(self, request, usuario_id, mes, anio):
         token = Token.objects.get(key=request.auth)
         if token:
             user = token.user
             group = user.groups.all().first().id
             if user.id == usuario_id or group == 1:
-                vencimientos = Vencimiento.objects.filter(
-                    propietario__id=usuario_id, fecha__gte=date.today())
-                serializer = VencimientoSerializer(vencimientos, many=True)
+                if 1 <= mes <= 12 and len(str(anio)) == 4:
+                    vencimientos = Vencimiento.objects.filter(
+                        propietario__id=usuario_id,
+                        fecha__month=mes,
+                        fecha__year=anio
+                    )
+                    
+                    serializer = VencimientoSerializer(vencimientos, many=True)
+                else:
+                    return Response({"error": "Mes o año proporcionados son inválidos"}, status=status.HTTP_400_BAD_REQUEST)
             else:
                 return Response({"error": "Acceso no autorizado"}, status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            return Response({"error": "Acceso no autorizado"}, status=status.HTTP_401_UNAUTHORIZED)
         return Response(serializer.data)
 
+    def handle_exception(self, exc):
+        # Imprimir el error
+        print("Error en la vista del programa:", exc)
+
+        # Devolver una respuesta de error al cliente
+        return Response(
+            {"detail": "Ocurrió un error en el servidor."},
+            status=status.HTTP_500_INTERNAL_SERVER_ERROR
+        )
 
 class VencimientoContaViewSet(viewsets.ModelViewSet):
     queryset = Vencimiento.objects.filter(fecha__gte=date.today())
