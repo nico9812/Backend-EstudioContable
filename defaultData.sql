e189f8dce57f867dffc46bd80e68a2e4b15dbb96
Barco Exequiel <exebarco280320@gmail.com>
2024-03-14 21:54:26 -0300

    db para render

--- defaultData.sql
+++ defaultData.sql
@@ -8,8 +8,10 @@ INSERT INTO documentos_categoria VALUES(4,'Seguro vehicular');
 INSERT INTO documentos_categoria VALUES(5,'Programas de higiene y seguridad');
 INSERT INTO documentos_categoria VALUES(6,'Otros');
 
-INSERT INTO users_customuser VALUES(1,'pbkdf2_sha256$720000$jIWuHgZbOCZQCIKDSJaaLY$Chn8MpW/2fW06H8eCI/BLT/9lHpMnaxiA0fGtlPTBrc=','2024-03-11 20:42:53.492520',1,'admin','','',1,1,'2024-03-11 20:42:19.218248',NULL,'',NULL);
-INSERT INTO users_customuser VALUES(2,'pbkdf2_sha256$720000$mDKtjXG2SMznlJncQHv4Vk$Jxv1Q8tw2wZYZs3HwQEyRdVLFTfLt6+2JdvTZHAe1iA=',NULL,0,'contador','','',0,1,'2024-03-11 20:43:36.125250',NULL,'',NULL);
+INSERT INTO users_customuser VALUES(1,'pbkdf2_sha256$720000$TAnV84FIT5E2v9pZxsUkmP$CZfa+h6ntMQeHY5ahMSXKNbzaXghbjWEgofcA0gkMl0=','2024-03-15 00:51:28.545680',1,'admin','','',1,1,'2024-03-15 00:51:24.274192',NULL,'',NULL);
+INSERT INTO users_customuser VALUES(2,'pbkdf2_sha256$720000$nu3iL5YGdwvF8Br2XuBv7c$1dwt+TDMs/RTR3eZWvXRq0xqOnS60lQA8QN2wcLNkkA=',NULL,0,'contador','','',0,1,'2024-03-15 00:52:00.670579',NULL,'',NULL);
+INSERT INTO users_customuser VALUES(3,'pbkdf2_sha256$720000$UGoz9q8z6ffM4V0Gioz7IV$f8UBk4RYJrPzuIY7I6YUwHbXKVumEYQdTrS7DJLNWw4=',NULL,0,'cliente','','',0,1,'2024-03-15 00:52:19.139511',NULL,'',NULL);
 
-INSERT INTO users_customuser_groups VALUES (1, 2, 1);
-INSERT INTO users_customuser_groups VALUES (2, 2, 2);
+INSERT INTO users_customuser_groups VALUES(1,1,2);
+INSERT INTO users_customuser_groups VALUES(3,2,1);
+INSERT INTO users_customuser_groups VALUES(4,3,2);
