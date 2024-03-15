43839b99a2626e1025bdc250a1acb67124e7e81d
Barco Exequiel <exebarco280320@gmail.com>
2024-03-14 23:15:23 -0300

    asdasd3

--- defaultData.sql
+++ defaultData.sql
@@ -12,6 +12,5 @@ INSERT INTO users_customuser VALUES(1,'pbkdf2_sha256$720000$TAnV84FIT5E2v9pZxsUk
 INSERT INTO users_customuser VALUES(2,'pbkdf2_sha256$720000$nu3iL5YGdwvF8Br2XuBv7c$1dwt+TDMs/RTR3eZWvXRq0xqOnS60lQA8QN2wcLNkkA=',NULL,0,'contador','','',0,1,'2024-03-15 00:52:00.670579',NULL,'',NULL);
 INSERT INTO users_customuser VALUES(3,'pbkdf2_sha256$720000$UGoz9q8z6ffM4V0Gioz7IV$f8UBk4RYJrPzuIY7I6YUwHbXKVumEYQdTrS7DJLNWw4=',NULL,0,'cliente','','',0,1,'2024-03-15 00:52:19.139511',NULL,'',NULL);
 
-INSERT INTO users_customuser_groups VALUES(1,1,2);
 INSERT INTO users_customuser_groups VALUES(3,2,1);
 INSERT INTO users_customuser_groups VALUES(4,3,2);
